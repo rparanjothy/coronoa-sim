270d5374d436cf216e20c67547619549d24e2de9
Paranjothy <rparanjo@amd.com>
2020-03-31 15:32:48 -0500

    now graphs

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var infectDelay = prompt("Time Delay to Quarentine", "5000");
+var infectDelay = prompt("Time Delay to Quarentine (ms)", "5000");
 class Vector {
   constructor(x, y) {
     this.x = x;
@@ -30,14 +30,30 @@ class Vector {
 
 class World {
   constructor(population) {
-    this.stats = [["NI", "Q", "D", "R", "S", "\n"].join(",")];
+    this.stats = [
+      [
+        "Newly Infected",
+        "Quarentined",
+        "Dead",
+        "Recovered",
+        "Unaffected",
+        "\n"
+      ].join(",")
+    ];
     this.population = population;
     this.s = this.population;
     this.i = 0;
     this.r = 0;
     this.people = [];
     for (let i = 0; i < this.population; i++) {
-      this.people.push(new Person(random(width), random(height)));
+      this.people.push(
+        new Person(
+          random(width * 0.25, width * 0.75),
+          // random(width * 0.25, width * 0.75),
+          // random(height * 0.125, height * 0.325)
+          random(height * 0.125, height * 0.325)
+        )
+      );
     }
   }
 }
@@ -54,10 +70,14 @@ class Person {
     this.moved = false;
     this.accelerate();
     this.home = new Vector(30, 30);
-    this.home1 = new Vector(windowWidth - 300, 30);
+    this.home1 = new Vector(windowWidth - 250, 30);
     this.p = random(1);
   }
-
+  reached(x) {
+    let d = (this.l.x - x.x) ** 2 + (this.l.y - x.y) ** 2;
+    // console.log(d);
+    return d < 10;
+  }
   move() {
     if (this.quarantine) {
       if (this.p <= 0.2) {
@@ -65,22 +85,30 @@ class Person {
         this.a = new Vector(0, 0);
         const sx = this.home.x - this.l.x;
         const sy = this.home.y - this.l.y;
-        const nsx = (sx / abs(sy)) * 2;
-        const nsy = (sy / abs(sy)) * 2;
+        const nsx = (sx / abs(sy)) * 12;
+        const nsy = (sy / abs(sy)) * 12;
         this.steerForce = new Vector(nsx, nsy);
         this.v = this.steerForce;
         this.steerForce = new Vector(0, 0);
+        // if (!this.reached(this.home)) {
         this.l.add(this.v);
+        // } else {
+        // this.l = this.home;
+        // }
       } else {
         this.a = new Vector(0, 0);
         const sx = this.home1.x - this.l.x;
         const sy = this.home1.y - this.l.y;
-        const nsx = (sx / abs(sy)) * 2;
-        const nsy = (sy / abs(sy)) * 2;
+        const nsx = (sx / abs(sy)) * 12;
+        const nsy = (sy / abs(sy)) * 12;
         this.steerForce = new Vector(nsx, nsy);
         this.v = this.steerForce;
         this.steerForce = new Vector(0, 0);
+        // if (!this.reached(this.home1)) {
         this.l.add(this.v);
+        // } else {
+        // this.l = this.home1;
+        // }
       }
     } else {
       this.l.add(this.v);
@@ -102,15 +130,15 @@ class Person {
   }
 
   accelerate() {
-    if (this.l.x >= width - 10) {
+    if (this.l.x >= width * 0.76 - 10) {
       // l=true
       this.reverseX();
     }
-    if (this.l.x < 10) {
+    if (this.l.x < width * 0.2 - 10) {
       // l=true
       this.reverseX();
     }
-    if (this.l.y > height - 10) {
+    if (this.l.y > height * 0.6 - 10) {
       // l=true
       this.reverseY();
     }
@@ -136,7 +164,7 @@ class Person {
 }
 
 function setup() {
-  createCanvas(windowWidth - 50, windowHeight - 50);
+  cvs = createCanvas(windowWidth - 50, windowHeight - 50);
   w = new World(750);
 }
 
@@ -178,31 +206,63 @@ function draw() {
   strokeWeight(1);
   fill(255);
   textSize(15);
-  text(`Population - ${w.people.length}`, windowWidth - 200, 20);
   const i = w.people.filter(e => e.infected && !e.quarantine).length;
   const q = w.people.filter(e => e.quarantine).length;
   const s = w.people.filter(e => !e.infected).length;
 
-  text(`Newly Infected - ${i}`, windowWidth - 200, 40);
-  text(`Quarentined - ${q}`, windowWidth - 200, 60);
-  text(`Safe - ${s}`, windowWidth - 200, 80);
   const d = w.people.filter(e => e.quarantine && e.p <= 0.2).length;
   const rec = q - d;
-  text(`Dead - ${d}`, windowWidth - 200, 100);
-  text(`Recovered - ${rec}`, windowWidth - 200, 120);
 
-  w.stats.push([i, q, d, rec, s].join(",") + "\n");
-
+  // w.stats.push([i, q, d, rec, s].join(",") + "\n");
+  w.stats.push([i, q, d, rec, s]);
   if (i === 0 && q !== 0) {
     noLoop();
-    writeFile(w.stats, `corona-sim-data-${infectDelay}.csv`);
+    // writeFile(w.stats, `corona-sim-data-${infectDelay}.csv`);
   }
+
+  w.stats.forEach((e, idx) => {
+    strokeWeight(1);
+    y = height - e[0] * (400 / height);
+    y1 = height - e[1] * (400 / height);
+    y2 = height - e[2] * (400 / height);
+    y3 = height - e[3] * (400 / height);
+    y4 = height - e[4] * (400 / height);
+    textSize(18);
+    stroke("white"); // Change the color
+    text(`Population - ${w.people.length}`, windowWidth - 400, height - 220);
+
+    stroke("red"); // Change the color
+    fill("black");
+    strokeWeight(2);
+    ellipse(idx + 100, parseInt(y), 2);
+    text(`Newly Infected - ${i}`, windowWidth - 400, height - 200);
+
+    stroke("yellow"); // Change the color
+    ellipse(idx + 100, parseInt(y1), 2);
+    text(`Quarentined - ${q}`, windowWidth - 400, height - 180);
+
+    stroke("gray"); // Change the color
+    ellipse(idx + 100, parseInt(y2), 2);
+    text(`Dead - ${d}`, windowWidth - 400, height - 160);
+
+    stroke("green"); // Change the color
+    ellipse(idx + 100, parseInt(y3), 2);
+    text(`Recovered - ${rec}`, windowWidth - 400, height - 140);
+
+    stroke("purple"); // Change the color
+    ellipse(idx + 100, parseInt(y4), 2);
+    text(`Safe - ${s}`, windowWidth - 400, height - 120);
+
+    stroke("white"); // Change the color
+    ellipse(idx + 100, height, 2);
+    line(100, height, 100, height - w.population * (400 / height));
+  });
 }
 
-// function mousePressed() {
-//   noLoop();
-// }
+function mousePressed() {
+  noLoop();
+}
 
-// function mouseReleased() {
-//   loop();
-// }
+function mouseReleased() {
+  loop();
+}
